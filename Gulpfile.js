6a75d4141913bd5ad6e3312688a1382783a208aa
Lucas <lucasvallenet@gmail.com>
2016-02-02 17:52:57 +0100

    Gulpfile update and node_moduls added in gitignore

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,105 +1,279 @@
-// Load plugins
-var gulp = require('gulp'),
-minifycss = require('gulp-minify-css'),
-jshint = require('gulp-jshint'),
-uglify = require('gulp-uglify'),
-imagemin = require('gulp-imagemin'),
-rename = require('gulp-rename'),
-clean = require('gulp-clean'),
-concat = require('gulp-concat'),
-notify = require('gulp-notify'),
-less   = require('gulp-less'),
-cache = require('gulp-cache'),
-livereload = require('gulp-livereload'),
+'use strict';
 
-// Styles
-gulp.task('styles', function() {
-  return gulp.src('app/styles/app.less')
-  .pipe(less())
-  .pipe(gulp.dest('dist/styles'))
-  .pipe(rename({ suffix: '.min' }))
-  .pipe(minifycss())
-  .pipe(connect.reload())
-  .pipe(notify({ message: 'Styles mis à jour' }));
+/* ================================
+ * Dependencies
+ * ================================ */
+
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var sourcemaps   = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var rename       = require('gulp-rename');
+var svgstore     = require('gulp-svgstore');
+var browserSync  = require('browser-sync').create();
+var notify       = require('gulp-notify');
+var scsslint     = require('gulp-scss-lint');
+var stylish      = require('gulp-scss-lint-stylish2');
+var cssmin       = require('gulp-cssmin');
+var jscs         = require('gulp-jscs');
+var jsStylish    = require('gulp-jscs-stylish');
+var imageOptim   = require('gulp-imageoptim');
+var uglify       = require('gulp-uglify');
+var insert       = require('gulp-insert');
+var noop         = function () {};
+
+
+function joinArray(array, before, after) {
+  var string = '';
+  for (var i = 0; i < array.length; i++) {
+    string += before + array[i] + after;
+  }
+  return string;
+}
+
+
+/* ================================
+ * Config variables
+ * ================================ */
+
+var docRoot = '';
+var assets = docRoot + 'assets/';
+
+// Sass options
+var sassOptions = {
+  src  : assets + 'scss/**/*.scss',
+  dest : assets + 'css',
+  opts : {
+    outputStyle  : 'expanded',
+    includePaths : ['./node_modules/sass-list-maps']
+  }
+};
+
+// SCSS Lint options
+var scssLintOptions = {
+  src  : [assets + 'scss/**/*.scss'],
+  opts : {
+    config    : '.scss-lint.yml',
+    maxBuffer : 10240000
+  }
+};
+
+var cssminOptions = {
+  src: [sassOptions.dest + '/*.css', sassOptions.dest + '/pages/*.css', sassOptions.dest + '/vendors/*.css'],
+  dest: sassOptions.dest,
+  opts: {
+    mediaMerging: false,
+    advanced: false,
+    aggressiveMerging: false,
+    processImport: false,
+    rebase: false,
+    restructuring: false,
+    roundingPrecision: -1
+  }
+};
+
+// JS Code Style options
+var jscsOptions = {
+  src  : [assets + 'js/*.js', assets + 'js/pages/*.js', assets + 'js/utils/*.js', assets + 'js/vendors/*.js'],
+  opts : {
+    config : '.jscsrc',
+  }
+};
+
+// Autoprefixer options
+var autoprefixerOptions = {
+  browsers : ['last 4 versions', '> 0.2%', 'Firefox ESR']
+};
+
+// SVGStore options
+var svgstoreOptions = {
+  src  : assets + 'svg/sources/*.svg',
+  dest : assets + 'svg',
+  name : 'sprite.svg',
+  opts : {
+    inlineSvg  : true,
+    formatting : {
+      indent_size : 2
+    }
+  }
+};
+
+// ImageOptim options
+var imageOptimOptions = {
+  src  : [assets + 'img/**/*.jpg'],
+  dest : assets + 'img'
+}
+
+// Notify options
+var notifyOptions = {
+  contentImage : __dirname + '/' + docRoot + '/android-chrome-96x96.png',
+  sound        : false
+};
+
+// Browser Sync options
+var browserSyncOptions = {
+  watchTask : true,
+  open      : false,
+  proxy     : 'localhost'
+};
+
+// Watch options
+var watchOptions = [
+  // surveille les fichiers php pour l'intégration
+  //docRoot + '/*.php',
+  //docRoot + '/includes/.*.php',
+  // surveille les fichiers js
+  assets + 'js/*.js',
+  assets + 'js/**/*.js',
+  // surveille les fichiers de template
+  assets + 'tpl/*.html',
+  assets + 'tpl/*.html'
+];
+
+
+
+/* ================================
+ * Tasks
+ * ================================ */
+
+
+// Sass compilation
+gulp.task('sass', function () {
+  notifyOptions.title   = 'Sass';
+  notifyOptions.message = 'Le fichier <%= file.relative %> a été mis à jour';
+
+  return gulp
+    .src(sassOptions.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions.opts).on('error', notify.onError({
+        message: '<%= error.message %>'
+      })).on('error', sass.logError))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest(sassOptions.dest))
+    .pipe(sourcemaps.write())
+    .pipe(browserSync.stream())
+    .pipe(notify(notifyOptions));
 });
 
-// assets
-gulp.task('assets', function() {
-  return gulp.src('app/assets/fonts')
-  .pipe(gulp.dest('dist/assets/fonts'))
-  .pipe(connect.reload())
-  .pipe(notify({ message: 'assets mis à jour' }));
-});
 
-// Scripts
-gulp.task('scripts', function() {
-  return gulp.src('app/scripts/**/*.js')
-  .pipe(jshint('.jshintrc'))
-  .pipe(jshint.reporter('default'))
-  .pipe(concat('app.js'))
-  .pipe(gulp.dest('dist/scripts'))
-  .pipe(rename({ suffix: '.min' }))
-  //.pipe(uglify())
-  .pipe(connect.reload())
-  .pipe(notify({ message: 'JS mis à jour' }));
-});
 
-// html
-gulp.task('html', function() {
-    gulp.src('app/*.html')
-    .pipe(gulp.dest('./dist'))
-    .pipe(notify({ message: 'HTML mis à jour' }));
+gulp.task('sassProd', function () {
+  notifyOptions.title   = 'Sass';
+  notifyOptions.message = 'Le fichier <%= file.relative %> a été mis à jour';
+
+  sassOptions.opts.outputStyle = 'compressed';
+
+  return gulp
+    .src(sassOptions.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions.opts).on('error', notify.onError({
+        message: '<%= error.message %>'
+      })).on('error', sass.logError))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest(sassOptions.dest))
+    .pipe(sourcemaps.write())
+    .pipe(browserSync.stream())
+    .pipe(notify(notifyOptions));
 });
 
 
-// views
-gulp.task('views', function() {
-    gulp.src('app/views/*')
-     .pipe(connect.reload())
-    .pipe(gulp.dest('dist/views/'));
+// SCSS Linting
+gulp.task('scss-lint', function() {
+  var reporter = stylish();
+  scssLintOptions.opts.customReporter = reporter.issues;
+
+  return gulp
+    .src(scssLintOptions.src)
+    .pipe(scsslint(scssLintOptions.opts))
+    .pipe(reporter.printSummary);
 });
 
-// data
-gulp.task('data', function() {
-    gulp.src('app/data/*')
-    .pipe(connect.reload())
-    .pipe(gulp.dest('dist/data/'));
-});
+// JS Code Style linting
+gulp.task('js-lint', function () {
 
-// Images
-gulp.task('images', function() {
-  return gulp.src('app/assets/img/*')
-  .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-  .pipe(connect.reload())
-  .pipe(gulp.dest('dist/assets/img'))
-  .pipe(notify({ message: 'Images mises à jour' }));
-});
-
-// Clean
-gulp.task('clean', function() {
-  return gulp.src(['dist/styles', 'dist/scripts', 'dist/images'], {read: false})
-  .pipe(clean());
-});
-
-//open browser
-gulp.task('openbrowser', function() {
-  opn( 'http://' + server.host + ':' + server.port );
+  return gulp.src(jscsOptions.src)
+    .pipe(jscs())
+    .on('error', noop)
+    .pipe(jsStylish())
 });
 
 
-// Default task
-gulp.task('default', ['clean', 'scripts', 'images', 'styles', 'html', 'views'], function() {
-  gulp.watch('styles', 'scripts', 'images');
+// Images optimization
+gulp.task('image-optim', function() {
+
+  return gulp
+  .src(imageOptimOptions.src)
+    .pipe(imageOptim.optimize())
+    .pipe(gulp.dest(imageOptimOptions.dest));
 });
+
+
+// Sprite SVG
+gulp.task('svgstore', function () {
+  notifyOptions.title   = 'SVG Store';
+  notifyOptions.message = 'Le fichier <%= file.relative %> a été mis à jour';
+
+  return gulp
+    .src(svgstoreOptions.src)
+    .pipe(rename({prefix: 'svg-'}))
+    .pipe(svgstore(svgstoreOptions.opts))
+    .pipe(rename(svgstoreOptions.name))
+    .pipe(gulp.dest(svgstoreOptions.dest))
+    .pipe(notify(notifyOptions));
+});
+
+// Minify CSS
+gulp.task('minify', function () {
+    gulp.src(cssminOptions.src)
+        .pipe(cssmin(cssminOptions.opts))
+        .pipe(gulp.dest(function(file) {
+          var path = file.base.replace('/css/', '/css/min/');
+          return path;
+        }));
+});
+
+// Uglify
+gulp.task('uglify', function() {
+  return gulp.src(jscsOptions.src)
+    .pipe(uglify({
+      preserveComments: function(node, comment) {
+        if (comment.value.substr(0,2) === '!!') {
+          return true;
+        } else {
+          return false;
+        }
+      },
+      compress: {
+        drop_console: true
+      }
+    }))
+    .pipe(gulp.dest(function(file) {
+      var path = file.base.replace('/js/', '/js/min/');
+      return path;
+    }));
+});
+
+
+// Compress
+gulp.task('compress', ['minify', 'uglify'], function() {
+  return gulp.src( assets +'js/min/vendors/console.js' )
+    .pipe(insert.append(joinArray(logs, 'console.log(', ');')))
+    .pipe(gulp.dest(function(file) {
+      return file.base;
+    }));
+});
+
 
 // Watch
-gulp.task('watch',['serve', 'scripts', 'images', 'styles', 'html', 'views', 'assets', 'data'], function(){
-    gulp.watch('app/styles/**/*.less', ['styles']);
-    gulp.watch('app/scripts/**/*.js', ['scripts']);
-    gulp.watch('app/views/*', ['views']);
-    gulp.watch('app/data/*', ['data']);
-    gulp.watch('app/assets/fonts/*', ['assets']);
-    gulp.watch('app/images/**/*', ['images']);
-    gulp.watch('app/*.html', ['html']);
-  });
+gulp.task('watch', ['svgstore', 'sass'], function() {
+  browserSync.init(browserSyncOptions);
+  console.log(__dirname);
 
+  gulp.watch(sassOptions.src, ['sass']);
+  gulp.watch(jscsOptions.src, ['js-lint']);
+  gulp.watch(svgstoreOptions.src, ['svgstore']);
+  gulp.watch(watchOptions).on('change', browserSync.reload);
+});
+
+
+gulp.task('default', ['watch']);
