d40e1e44c18dfb972ea02579da20f8d4dd552c2e
IsaBartkowiak <bartkowiak.isadora@gmail.com>
2016-02-03 21:55:25 +0100

    Add modal on new services

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,43 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+// Pop up envoyer une demande de service
+// $(document).on('click','a',function(event){
+// 	event.preventDefault();
+// 	link = $(this).attr('href');
+// 	$("#wait").fadeIn(900,function(){;
+// 		$("#content").hide();
+// 		Turbolinks.visit(link);
+// 	});
+// })
+
+$( document ).ready(function() {
+
+	var popup = $( ".popup" );
+	var button = $( ".btn-ask" );
+	var popupClose = $('.popup-close');
+
+	function openPopup(){
+		popup.addClass('_active');
+		button.addClass('is-active');
+	}
+	function closePopup(){
+		popup.removeClass('_active');
+		button.removeClass('is-active');
+	}
+	button.click(function() {
+		var target = $(this).attr('data-link');
+		$('.popup-content').load( target+'.js',  openPopup);
+	});
+	popupClose.click(function() {
+		closePopup();
+	});
+	$(document).mouseup(function (e){
+		var popupContent = $( ".popup-content" )
+		if (!popupContent.is(e.target) && popupContent.has(e.target).length === 0) {
+			closePopup();
+		}
+	});
+});
+
