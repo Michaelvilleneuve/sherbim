c7d7b49c45661b8ba87d2e21f59b8e49db67f895
Michael Villeneuve <contact@michaelvilleneuve.fr>
2016-02-03 11:48:05 +0100

    Added scaffold for comments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def index
+  	@services = Service.all
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160203080619) do
+ActiveRecord::Schema.define(version: 20160203104716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "user_id"
+    t.integer  "mark"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "services", force: :cascade do |t|
     t.integer  "user_id"
@@ -48,5 +59,6 @@ ActiveRecord::Schema.define(version: 20160203080619) do
     t.datetime "oauth_expires_at"
   end
 
+  add_foreign_key "comments", "users"
   add_foreign_key "services", "users"
 end
